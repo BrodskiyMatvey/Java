a9cccb83d05224f06ee6d25334b4e34946cee521
BrodskiyMatvey <brodskiymatvey@mail.ru>
2021-02-17 22:54:02 +0200

    fixed hw2 and create MainLesson3

--- src/main/java/lesson2/Hw2.java
+++ src/main/java/lesson2/Hw2.java
@@ -10,6 +10,8 @@ public class Hw2 {
 //        task3();
 //        task4();
 //        task5();
+//        System.out.println(task6(new int[]{1, 3, 1, 5}));
+//        task7(new int[]{1, 2, 3, 4, 5}, 1);
     }
 
     public static void example() {
@@ -32,14 +34,16 @@ public class Hw2 {
     public static void task1() {
         int[] arr1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         for (int i = 0; i < arr1.length; i++) {
-            switch (arr1[i]) {
+            /*switch (arr1[i]) {
                 case 0:
                     arr1[i] = 1;
                     break;
                 case 1:
                     arr1[i] = 0;
                     break;
-            }
+            }*/
+
+            arr1[i] = (arr1[i] == 0) ? 1 : 0; //Alternative
 
             System.out.print(arr1[i] + " ");
         }
@@ -48,9 +52,8 @@ public class Hw2 {
 
     public static void task2() {
         int[] arr2 = new int[8];
-        int n = 0;
         for (int i = 0; i < arr2.length; i++) {
-            arr2[i] = n++ * 3;
+            arr2[i] = i * 3;
             System.out.print(arr2[i] + " ");
         }
 
@@ -62,11 +65,8 @@ public class Hw2 {
 
             if (arr3[i] < 6) {
 
-                arr3[i] *= 2;
-
-            } else {
-
-                arr3[i] *= 1;
+//                arr3[i] *= 2;
+                arr3[i] = arr3[i] * 2; //Alternative
 
             }
 
@@ -76,37 +76,83 @@ public class Hw2 {
 
     public static void task4() {
         int[][] arr4 = new int[5][5];
-        int n;
+        int lastIndex = arr4.length - 1;
         for (int i = 0; i < arr4.length; i++) {
-            for (int j = 0; j < arr4[i].length; j++) {
+            arr4[i][i] = arr4[i][lastIndex - i] = 1;
 
-                if (arr4[i] == arr4[j]) {
-
-                    n = 1;
-
-                    arr4[i][j] = n;
-
-                } else {
-                    n = 0;
-                }
-
-
-//                System.out.print(arr4[i][j] + "\t"); Alternative array output
-                System.out.printf("%4d", arr4[i][j]);
-            }
-//            System.out.println(Arrays.toString(arr4[i])); Alternative array output
-            System.out.println();
+            System.out.println(Arrays.toString(arr4[i]));
         }
-    }  //Not ready
+    }
 
     public static void task5() {
         int[] arr5 = {5, 1, 7, 4, 11, 9};
         for (int i = 0; i < arr5.length; i++) {
             Arrays.sort(arr5);
-
         }
+
         System.out.println("Минимальное число: " + arr5[0]);
         System.out.println("Максимальное число: " + arr5[arr5.length - 1]);
+
+    }
+
+    public static boolean task6(int[] arr6) {
+        int leftSum = 0;
+        int rightSum = sum(arr6, 0, arr6.length);
+        boolean result = false;
+
+        if (rightSum % 2 != 0) {
+            return false;
+        }
+
+        for (int i = 0; i < arr6.length; i++) {
+            leftSum += arr6[i];
+            rightSum -= arr6[i];
+
+            if (leftSum == rightSum) {
+                result = true;
+            } else if (leftSum > rightSum) {
+                return result;
+            }
+        }
+        return result;
+    }
+
+    public static int sum(int[] arr6, int start, int end) {
+        int sum = 0;
+        for (int i = start; i < end; i++) {
+            sum += arr6[i];
+        }
+        return sum;
+    }
+
+    public static void task7(int[] arr7, int n) {
+        int shiftNumber = n % arr7.length;
+        if (shiftNumber < 0) {
+            shiftLeft(arr7, shiftNumber);
+        } else {
+            shiftRight(arr7, shiftNumber);
+        }
+        System.out.println(Arrays.toString(arr7) + "; shiftNumber = " + n);
+    }
+
+    public static void shiftRight(int[] arr7, int n) {
+        for (int i = 0; i < Math.abs(n); i++) {
+            int lastValue = arr7[arr7.length - 1];
+            for (int j = arr7.length - 1; j > 0; j--) {
+                arr7[j] = arr7[j - 1];
+            }
+            arr7[0] = lastValue;
+        }
+    }
+
+    public static void shiftLeft(int[] arr7, int n) {
+        for (int i = 0; i < Math.abs(n); i++) {
+            int firstValue = arr7[0];
+            for (int j = 0; j < arr7.length; j++) {
+                arr7[j] = arr7[j + 1];
+            }
+            arr7[0] = firstValue;
+        }
     }
 
 }
---
+++ src/main/java/lesson3/MainLesson3.java
@@ -0,0 +1,132 @@
+package lesson3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class MainLesson3 {
+    public static void main(String[] args) throws IOException {
+//        testString();
+//        int[] arr = {0, 0, 0, 0, 0};
+//        testArrayChange(arr.clone());
+//        testArrayChange(arr.clone());
+//        System.out.println(Arrays.toString(arr));
+
+//        testOverload(3, 4);
+//        testOverload(4, 5);
+//        testOverload(2.4, 6.2);
+//        testOverload(7);
+//        testOverload(7.4);
+//        testOverload(new int[]{1, 2, 3, 4, 5, 6, 7});
+//        testOverload(1, 2, 3, 4, 5, 6, 7);
+
+//        testConsoleInput();
+
+//        exampleFromGoogleConsoleInputInt();
+//        exampleFromGoogleConsoleInputString();
+//        exampleFromGoogleConsoleInputTheFirstWord();
+
+//        testRandom();
+    }
+
+
+    public static void testString() {
+        String str1 = "Java";
+        String str2 = "Java";
+        String str3 = new String("Java");
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+    }
+
+    public static void testArrayChange(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += i;
+        }
+        System.out.println("method: " + Arrays.toString(arr));
+    }
+
+    private static void testOverload(int a, int b) {
+        int multiple = a * b;
+        System.out.printf("Произведение чисел: %d * %d = %d(int)%n", a, b, multiple);
+    }
+
+    private static void testOverload(double a, double b) {
+        double multiple = a * b;
+        System.out.printf("Произведение чисел: %.1f * %.1f = %.1f(double)%n", a, b, multiple);
+    }
+
+    private static void testOverload(double a) {
+        double multiple = a * a;
+        System.out.printf("Квадрат числа: %.1f = %.1f(double)%n", a, multiple);
+    }
+
+    private static void testOverload(int a) {
+        testOverload((double) a);
+    }
+
+    private static void testOverload(int... a) {
+        int multiple = 1;
+        for (int i = 0; i < a.length; i++) {
+            multiple *= a[i];
+        }
+        System.out.printf("Произведение всех чисел: %d%n", multiple);
+    }
+
+    private static void exampleFromGoogleConsoleInputInt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите 2 любых целых числа: ");
+        int number1 = sc.nextInt();//считывает первое число и присваивает значение в number1
+        int number2 = sc.nextInt();//считывает второе число и присваивает значение в number2
+        System.out.print(number1 + number2);//выводит сумму number1 + number2
+    }
+
+    private static void exampleFromGoogleConsoleInputString() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите любое слово или фразу: ");
+        String phrase1 = sc.nextLine();
+        System.out.println(phrase1);
+    }
+
+    private static void exampleFromGoogleConsoleInputTheFirstWord() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите любое слово или фразу: ");
+        String phrase1 = sc.next();
+        System.out.println(phrase1);
+    }
+
+    private static void testConsoleInput() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.hasNextInt() ? scanner.nextInt() : 0;
+        if (a == 0) {
+            scanner.next();
+        }
+        int b = scanner.nextInt();
+
+        testOverload(a, b);
+
+    }
+
+    private static void testRandom() {
+        Random random = new Random();
+
+        int[] data = new int[30];
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = random.nextInt(10); //0 - 9
+            data[i] = random.nextInt(8) + 3; //3 - 10
+            data[i] = random.nextInt(12) - 5; //-5 - +6
+
+            data[i] = (int) (Math.random() * 10); //0 - 9
+            data[i] = (int) (Math.random() * 8) + 3; //3 - 10
+            data[i] = (int) (Math.random() * 12) -5; //-5 - +6
+        }
+
+        System.out.println(Arrays.toString(data));
+    }
+
+
+}
